d3ac99bee79e3bf50abcf2bda6ed4a19a43698d1
David Canino <canino.david@gmail.com>
2016-12-09 01:24:38 +0100

    added the xcode building infrastructure for the 'Example-001-old' test, and updating this latter a little bit

--- Example-001-old/src/main.cpp
+++ Example-001-old/src/main.cpp
@@ -1,13 +1,38 @@
+/*******************************************************************************************************************************************************
+ * David Canino (canino.david@gmail.com)
+ *
+ * Main website (GitHub): http://github.com/davidcanino/OpenGLExamples
+ * 
+ * Last update: December 2016
+ *
+ * This program is Free Software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.                                       
+ *                                                                         
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License (http://www.gnu.org/licenses/gpl.txt) for more details.
+ * 
+ * main.cpp - the main function for the 'Example-001 (Old Mode)' example
+ *******************************************************************************************************************************************************/
 
+/* First, we must understand which platform we are using. */
+#include <cstdlib>
 #ifdef __APPLE__
+
+	/* We are using a MacOSX platform (Macintosh) */
+	#include "GL/glew.h"
+	#include "GLUT/glut.h"
+	#include "OpenGL/gl.h"
 
 #else
 
+	/* We are not using a MacOSX platform. Thus, we have a generic Unix-like platform, like the GNU Linux, or a Microsoft Windows platform. */
 	#include "GL/glew.h"
 	#include "GL/glut.h"
 	#include "GL/gl.h"
+
 #endif
 
+/// This function draws a new window with the black background, and nothing else.
 void draw(void)
 {
 	/* We set a black background for the new window! */
@@ -16,6 +41,7 @@ void draw(void)
 	glFlush();
 }
 
+/// The main function for the <i>'Example-001 (Old Mode)'</i> example.
 int main(int argc,char **argv)
 {
 	/* We initialize everything, and create a very basic window! */
@@ -28,4 +54,3 @@ int main(int argc,char **argv)
 	glutMainLoop();
 	return EXIT_SUCCESS;
 }
-
