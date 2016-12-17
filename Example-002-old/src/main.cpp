e509c292d580369659220c3b474096f3dc8f8683
David Canino <canino.david@gmail.com>
2016-12-17 22:54:51 +0100

    added the souce code and the cmake building infrastructure for the 'Example-002 (Old Mode)' Example

---
+++ Example-002-old/src/main.cpp
@@ -0,0 +1,137 @@
+/*******************************************************************************************************************************************************
+ * David Canino (canino.david@gmail.com)
+ *
+ * Main website (GitHub): http://github.com/davidcanino/OpenGLExamples
+ * 
+ * Last update: December 2016
+ *
+ * This program is Free Software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.                                       
+ *                                                                         
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License (http://www.gnu.org/licenses/gpl.txt) for more details.
+ * 
+ * main.cpp - the main function for the 'Example-002 (Old Mode)' example
+ *******************************************************************************************************************************************************/
+
+/* First, we must understand which platform we are using. */
+#include <cstdlib>
+#ifdef __APPLE__
+
+	/* We are using a MacOSX platform (Macintosh) */
+	#include "GL/glew.h"
+	#include "GLUT/glut.h"
+	#include "OpenGL/gl.h"
+
+#else
+
+	/* We are not using a MacOSX platform. Thus, we have a generic Unix-like platform, like the GNU Linux, or a Microsoft Windows platform. */
+	#include "GL/glew.h"
+	#include "GL/glut.h"
+	#include "GL/gl.h"
+
+#endif
+
+/* Prototypes for all functions of interest! */
+void draw();
+void initialize();
+void resize(int w,int h);
+void manageKeys(unsigned char key, int x, int y);
+
+/// The main function for the <i>'Example-002 (Old Mode)'</i> example.
+int main(int argc,char **argv)
+{
+	/* We initialize everything, and create a very basic window! */
+	glutInit(&argc,argv);
+	
+	//#ifdef __APPLE__
+	
+		/* We assume to exploit the GLUT version in conjunction with the XCode. Thus, the following functions are not defined:
+		 *
+		 * -) glutInitContextVersion()
+		 * -) glutInitContextProfile() */
+
+	//#else
+	
+		/* Here, we are not using the GLUT version, provided by the XCode. Thus, the functions, mentioned above, should be defined. */
+	//	glutInitContextVersion(4,3);
+	//	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+
+	// #endif
+	
+	/* If we arrive here, we can continue! */
+	glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE);
+	glutInitWindowPosition(50,25);
+	glutInitWindowSize(480,480);
+	glutCreateWindow("The 'Example-002' Example, based on the (Old Mode) OpenGL");
+	glutDisplayFunc(draw);
+	glutReshapeFunc(resize);
+	glutKeyboardFunc(manageKeys);
+	glewExperimental = GL_TRUE;
+   	glewInit();
+   	initialize(); 
+	glutMainLoop();
+	return EXIT_SUCCESS;
+}
+
+/// This function updates the viewport for the scene when it is resized. */
+void resize(int w, int h)
+{
+	/* We update the projections and the modeling matrices! */
+	glViewport(0, 0, w, h);
+   	glMatrixMode(GL_PROJECTION);
+   	glLoadIdentity();
+   	glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
+   	glMatrixMode(GL_MODELVIEW);
+   	glLoadIdentity();
+}
+
+/// This function draws a new window with the black background, and nothing else.
+void draw()
+{
+	/* We draw a quadrilateral in the window! */
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0.0, 0.0);
+  	glBegin(GL_POLYGON);
+    glVertex3f(30.0, 20.0, 0.0);
+    glVertex3f(80.0, 20.0, 0.0);
+    glVertex3f(80.0, 80.0, 0.0);
+    glVertex3f(20.0, 80.0, 0.0);
+   	glEnd();
+	glFlush(); 
+}
+
+/// This function initializes the window. It draws a white background. */
+void initialize() { glClearColor(1.0, 1.0, 1.0, 0.0); }
+
+// Keyboard input processing routine.
+void manageKeys(unsigned char key, int x, int y)
+{
+	/* We are interested only in the 'q' - 'Q' - 'Esc' keys */
+	switch (key)
+	{
+		case 113:
+	
+		/* The key is 'q' */
+		exit(EXIT_SUCCESS);
+		break;
+		
+		case 81:
+	
+		/* The key is 'Q' */
+		exit(EXIT_SUCCESS);
+		break;
+		
+		case 27:
+	
+		/* The key is 'Esc' */
+		exit(EXIT_SUCCESS);
+		break;
+	
+		default:
+    
+    	/* Other keys are not important for us */
+    	break;
+	}	
+}
+	
