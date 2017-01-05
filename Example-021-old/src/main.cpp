41505d83c0aa217e06ff2e42a5dd875bb29e631c
David Canino <canino.david@gmail.com>
2017-01-06 00:03:18 +0100

    added the source code, the cmake, and the xcode building infrastructure for the 'Example-021-old' test

---
+++ Example-021-old/src/main.cpp
@@ -0,0 +1,192 @@
+/*******************************************************************************************************************************************************
+ * David Canino (canino.david@gmail.com)
+ *
+ * Main website (GitHub): http://github.com/davidcanino/OpenGLExamples
+ * 
+ * Last update: January 2017
+ *
+ * This program is Free Software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.                                       
+ *                                                                         
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License (http://www.gnu.org/licenses/gpl.txt) for more details.
+ * 
+ * main.cpp - the main function for the 'Example-021 (Old Mode)' example
+ *******************************************************************************************************************************************************/
+
+/* First, we must understand which platform we are using. */
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+#define PI 3.14159265358979324
+using namespace std;
+#ifdef __APPLE__
+
+	/* We are using a MacOSX platform (Macintosh) */
+	#include "GL/glew.h"
+	#include "GLUT/glut.h"
+	#include "OpenGL/gl.h"
+
+#else
+
+	/* We are not using a MacOSX platform. Thus, we have a generic Unix-like platform, like the GNU Linux, or a Microsoft Windows platform. */
+	#include "GL/glew.h"
+	#include "GL/glut.h"
+	#include "GL/gl.h"
+
+#endif
+
+/// The radius for the circle of interest.
+float radius;
+
+/// The coordinates of the center for the circle of interest.
+float xc,yc;
+
+/// The number of the samples, used for drawing the circle of interest.
+unsigned int num_samples;
+
+/* Prototypes for all functions of interest! */
+void draw();
+void initialize();
+void resize(int w,int h);
+void manageKeys(unsigned char key, int x, int y);
+
+/// The main function for the <i>'Example-021 (Old Mode)'</i> example.
+int main(int argc,char **argv)
+{
+	/* We initialize everything, and create a very basic window! */
+	cout<<endl<<"\tThis is the 'Example-021' Example, based on the (Old Mode) OpenGL"<<endl<<endl;
+	cout.flush();
+	cout<<"\tPlease, insert the radius R (positive and not null) for the circle of interest: ";
+	cin>>radius;
+	if( (!cin) || (radius<=0) )
+	{
+		cout<<"\tPLEASE, INSERT A VALID VALUE FOR THE RADIUS OF INTEREST. THIS PROGRAM IS CLOSING..."<<endl<<endl;
+		cout.flush();
+		return EXIT_FAILURE;
+	}
+	
+	/* Now, we read the coordinates for the center! */
+	cout<<"\tPlease, insert the coordinates (xc,yc) of the center for the circle of interest: ";
+	cout.flush();
+	cin>>xc>>yc;
+	if(!cin)
+	{
+		cout<<"\tPLEASE, INSERT THE COORDINATES (xc,yc) OF THE CENTER FOR THE CIRCLE OF INTEREST. THIS PROGRAM IS CLOSING..."<<endl<<endl;
+		cout.flush();
+		return EXIT_FAILURE;
+	}
+	
+	/* If we arrive here, we can draw our circle! */
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE);
+	glutInitWindowPosition(0,0);
+	glutInitWindowSize(500,500);
+	glutCreateWindow("The 'Example-021' Example, based on the (Old Mode) OpenGL");
+	glutDisplayFunc(draw);
+	glutReshapeFunc(resize);
+	glutKeyboardFunc(manageKeys);
+	glewExperimental = GL_TRUE;
+   	glewInit();
+   	initialize(); 
+	glutMainLoop();
+	return EXIT_SUCCESS;
+}
+
+/// This function initializes the OpenGL window of interest.
+void initialize() 
+{
+	/* We initialize the OpenGL window of interest! */	
+	cout<<endl<<"\tWe draw a circle having center ("<<xc<<","<<yc<<") and radius R="<<radius<<endl<<endl;
+	cout.flush();
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	num_samples=3;
+}
+
+/// This function updates the viewport for the scene when it is resized. */
+void resize(int w, int h)
+{
+	/* We update the projections and the modeling matrices! */
+	glViewport(0, 0, w, h);
+   	glMatrixMode(GL_PROJECTION);
+   	glLoadIdentity();
+   	glOrtho(xc-1.1*radius,xc+1.1*radius,yc-1.1*radius,yc+1.1*radius,-1,1);
+   	glMatrixMode(GL_MODELVIEW);
+   	glLoadIdentity();
+}
+
+/// This function is the keyboard input processing routine for the OpenGL window of interest.
+void manageKeys(unsigned char key, int x, int y)
+{
+	/* We are interested only in the 'q' - 'Q' - 'Esc' - '+' - '-' keys */
+	switch (key)
+	{
+		case 113:
+	
+		/* The key is 'q' */
+		cout<<endl;
+		cout.flush();
+		exit(EXIT_SUCCESS);
+		break;
+		
+		case 81:
+	
+		/* The key is 'Q' */
+		cout<<endl;
+		cout.flush();
+		exit(EXIT_SUCCESS);
+		break;
+		
+		case 27:
+	
+		/* The key is 'Esc' */
+		cout<<endl;
+		cout.flush();
+		exit(EXIT_SUCCESS);
+		break;
+		
+		case '+':
+		
+		/* The key is '+', thus we increase the number of the samples! */
+		num_samples=num_samples+1;
+		glutPostRedisplay();
+		break;
+		
+		case '-':
+		
+		/* The key is '-', thus we decrease the number of the samples! */
+		if(num_samples>3) num_samples=num_samples-1;
+		else cout<<"\tThe minimum number 3 of samples is reached"<<endl;
+		cout.flush();
+		glutPostRedisplay();
+		break;
+
+		default:
+
+    	/* Other keys are not important for us */
+    	break;
+	}
+}
+
+/// This function draws the polyline, approximating the circle of interest, in the OpenGL window of interest.
+void draw()
+{
+	float t;
+
+	/* We draw the polyline, approximating the circle of interest, in the OpenGL window of interest. */
+	t=0;
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0,0.0,0.0);
+	glBegin(GL_LINE_LOOP);
+	for(unsigned int i=0;i<=num_samples;i++)
+	{
+		glVertex3f(xc+radius*cos(t),yc+radius*sin(t),0);
+		t=t+(2*PI/num_samples);
+	}
+	
+	/* If we arrive here, all is ok */
+	glEnd();
+	glFlush();
+	cout<<"\tApproximated and drawn the circle of interest with "<<num_samples<<" samples"<<endl;
+	cout.flush();
+}
