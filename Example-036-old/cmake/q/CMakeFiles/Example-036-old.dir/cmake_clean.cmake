45bfb98fcfb05ba5ba8203252eb37ea38d98fd77
David Canino <canino.david@gmail.com>
2017-01-19 00:34:17 +0100

    added the source code, as well as the xcode and the cmake building infrastructure for the 'Example-036-old' test

---
+++ Example-036-old/cmake/q/CMakeFiles/Example-036-old.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Example-036-old.dir/Users/david/OpenGLExamples/Example-036-old/src/main.cpp.o"
+  "bin/Example-036-old.pdb"
+  "bin/Example-036-old"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Example-036-old.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
