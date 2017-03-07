1c0bf2d0071e40d5da28172678590c5ae344d0c8
David Canino <canino.david@gmail.com>
2017-03-07 14:26:10 +0100

    Updated and modified several tests

--- Example-012-old/src/main.cpp
+++ Example-012-old/src/main.cpp
@@ -11,7 +11,7 @@
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License (http://www.gnu.org/licenses/gpl.txt) for more details.
  * 
- * main.cpp - the main function for the 'Example-012 (Old Mode)' example
+ * main.cpp - the main function for the 'Example-012 (Old Mode)' example.
  *******************************************************************************************************************************************************/
 
 /* First, we must understand which platform we are using. */
@@ -20,7 +20,7 @@
 using namespace std;
 #ifdef __APPLE__
 
-	/* We are using a MacOSX platform (Macintosh) */
+	/* We are using a MacOSX platform (Macintosh). */
 	#include "GL/glew.h"
 	#include "GLUT/glut.h"
 	#include "OpenGL/gl.h"
@@ -40,12 +40,21 @@ void initialize();
 void resize(int w,int h);
 void manageKeys(unsigned char key, int x, int y);
 GLenum face,mode;
-bool eol=false;
 
 /// The main function for the <i>'Example-012 (Old Mode)'</i> example.
 int main(int argc,char **argv)
 {
 	/* We initialize everything, and create a very basic window! */
+	cout<<endl<<"\tThis is the 'Example-012' Example, based on the (Old Mode) OpenGL."<<endl;
+	cout<<"\tIt draws a custom shape, approximated by a triangle strip, in the scene, and it allows to modify its rendering as follows:"<<endl<<endl;
+	cout<<"\t-) only the 'front' side of the triangles in the triangle strip of interest is rendered by pressing the 'f' and 'F' keys;"<<endl;
+	cout<<"\t-) only the 'back' side of the triangles in the triangle strip of interest is rendered by pressing the 'b' and 'B' keys;"<<endl;
+	cout<<"\t-) both the sides of the triangles in the triangle strip of interest are rendered by pressing the 't' and 'T' keys;"<<endl;
+	cout<<"\t-) only the vertices of the triangles in the triangle strip of interest are rendered by pressing the 'p' and 'P' keys;"<<endl;
+	cout<<"\t-) only the edges of the triangles in the triangle strip of interest are rendered (wireframe version) by pressing the 'l' and the 'L' keys;"<<endl;
+	cout<<"\t-) the triangles in the triangle strip are entirely rendered (filled version) by pressing the 'i' and 'I' keys."<<endl<<endl;
+	cout<<"\tIt is possible to end this program by pressing one among the 'Q' - 'q' - 'Esc' keys."<<endl<<endl;
+	cout.flush();
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE);
 	glutInitWindowPosition(0,0);
@@ -93,181 +102,160 @@ void draw()
 	glEnd();
 	glFlush();
 	glFlush();
+	if(face==GL_FRONT)
+	{
+		/* We analyze only the 'front' side of the triangles in the triangle strip, approximating the custom shape of interest. */
+		if(mode==GL_POINT) cout<<"\tOnly the points in the 'front' side of the triangles in the triangle strip, approximating the custom shape of interest, are rendered in the current scene."<<endl;
+		if(mode==GL_LINE) cout<<"\tOnly the edges in the 'front' side of the triangles in the triangle strip, approximating the custom shape of interest, are rendered (wireframe version) in the current scene."<<endl;
+		if(mode==GL_FILL) cout<<"\tOnly the 'front' side of the triangles in the triangle strip, approximating the custom shape of interest, is completely rendered (filled version) in the current scene."<<endl;
+		cout.flush();
+	}
+	else if(face==GL_BACK)
+	{
+		/* We analyze only the 'back' side of the triangles in the triangle strip, approximating the custom shape of interest. */
+		if(mode==GL_POINT) cout<<"\tOnly the points in the 'back' side of the triangles in the triangle strip, approximating the custom shape of interest, are rendered in the current scene."<<endl;
+		if(mode==GL_LINE) cout<<"\tOnly the edges in the 'back' side of the triangles in the triangle strip, approximating the custom shape of interest, are rendered (wireframe version) in the current scene."<<endl;
+		if(mode==GL_FILL) cout<<"\tOnly the 'back' side of the triangles in the triangle strip, approximating the custom shape of interest, is completely rendered (filled version) in the current scene."<<endl;
+		cout.flush();
+	}
+	else
+	{
+		/* We analyze both the sides of the triangles in the triangle strip, approximating the custom shape of interest. */
+		if(mode==GL_POINT) cout<<"\tOnly the points in both the 'front' and the 'back' sides of the triangles in the triangle strip, approximating the custom shape of interest, are rendered in the current scene."<<endl;
+		if(mode==GL_LINE) cout<<"\tOnly the edges in both the 'front' and the 'back' sides of the triangles in the triangle strip, approximating the custom shape of interest, are rendered (wireframe version) in the current scene."<<endl;
+		if(mode==GL_FILL) cout<<"\tBoth the 'front' and the 'back' sides of the triangles in the triangle strip, approximating the custom shape of interest, are completely rendered (filled version) in the current scene."<<endl;
+		cout.flush();
+	}
 }
 
 /// This function initializes the OpenGL window of interest.
 void initialize() 
 {
 	/* We initialize the OpenGL window of interest! */
-	cout<<endl<<"\tThis is the 'Example-012' Example, based on the (Old Mode) OpenGL."<<endl;
-	cout<<"\tIt draws a custom shape, approximated by a triangle strip, in the scene, and it allows to modify its rendering as follows:"<<endl<<endl;
-	cout<<"\t-) only the 'front' side of the triangles in the triangle strip is rendered by pressing the 'f' and 'F' keys;"<<endl;
-	cout<<"\t-) only the 'back' side of the triangles in the triangle strip is rendered by pressing the 'b' and 'B' keys;"<<endl;
-	cout<<"\t-) both the sides of the triangles in the triangle strip are rendered by pressing the 't' and 'T' keys;"<<endl;
-	cout<<"\t-) only the vertices of the triangles in the triangle strip are rendered by pressing the 'p' and 'P' keys;"<<endl;
-	cout<<"\t-) only the edges of the triangles in the triangle strip (wireframe version) are rendered by pressing the 'l' and the 'L' keys;"<<endl;
-	cout<<"\t-) the triangles in the triangle strip are entirely rendered (filled version) by pressing the 'i' and 'I' keys."<<endl<<endl;
-	cout<<"\tIt is possible to end this program by pressing one among the 'Q' - 'q' - 'Esc' keys."<<endl<<endl;
-	cout.flush();
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 	face=GL_FRONT;
 	mode=GL_FILL;
-	eol=false;
+	cout<<"\tInitially, only the 'front' side of the triangles in the triangle strip, approximating the custom shape of interest, is completely filled (filled version) in the current scene."<<endl<<endl;
+	cout.flush();
 }
 
 /// This function is the keyboard input processing routine for the OpenGL window of interest.
 void manageKeys(unsigned char key, int x, int y)
 {
-	/* We are interested only in the 'q' - 'Q' - 'Esc' - 'f' - 'F' - 'b' - 'F' - 't' - 'T' - 'i' - 'I' - 'l' - 'L' - 'P' - 'p' keys */
+	/* We are interested only in the 'q' - 'Q' - 'Esc' - 'f' - 'F' - 'b' - 'F' - 't' - 'T' - 'i' - 'I' - 'l' - 'L' - 'P' - 'p' keys. */
 	switch (key)
 	{
 		case 'q':
 	
-		/* The key is 'q' */
-		if(eol) cout<<endl;
+		/* The key is 'q', thus we can exit from this program. */
+		cout<<endl;
 		cout.flush();
 		exit(EXIT_SUCCESS);
 		break;
 		
 		case 'Q':
 	
-		/* The key is 'Q' */
-		if(eol) cout<<endl;
+		/* The key is 'Q', thus we can exit from this program. */
+		cout<<endl;
 		cout.flush();
 		exit(EXIT_SUCCESS);
 		break;
 		
 		case 27:
 	
-		/* The key is 'Esc' */
-		if(eol) cout<<endl;
+		/* The key is 'Esc', thus we can exit from this program. */
+		cout<<endl;
 		cout.flush();
 		exit(EXIT_SUCCESS);
 		break;
 		
 		case 'F':
 
-		/* The key is 'F' */
+		/* The key is 'F', thus we draw only the 'front' side of the triangles in the triangle strip, approximating the custom shape of interest. */
 		face=GL_FRONT;
-		cout<<"\tRendering only the 'front' side of the triangles in the triangle strip."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		case 'f':
 
-		/* The key is 'f' */
+		/* The key is 'f', thus we draw only the 'front' side of the triangles in the triangle strip, approximating the custom shape of interest. */
 		face=GL_FRONT;
-		cout<<"\tRendering only the 'front' side of the triangles in the triangle strip."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		case 'B':
 
-		/* The key is 'B' */
+		/* The key is 'B', thus we draw only the 'back' side of the triangles in the triangle strip, approximating the custom shape of interest. */
 		face=GL_BACK;
-		cout<<"\tRendering only the 'back' side of the triangles in the triangle strip."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		case 'b':
 
-		/* The key is 'b' */
+		/* The key is 'b', thus we draw only the 'back' side of the triangles in the triangle strip, approximating the custom shape of interest. */
 		face=GL_BACK;
-		cout<<"\tRendering only the 'back' side of the triangles in the triangle strip."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		case 'T':
 
-		/* The key is 'T' */
+		/* The key is 'T', thus we draw both the 'front' and the 'back' sides of the triangles in the triangle strip, approximating the custom shape of interest. */
 		face=GL_FRONT_AND_BACK;
-		cout<<"\tRendering both the 'front' and the 'back' sides of the triangles in the triangle strip."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		case 't':
 
-		/* The key is 't' */
+		/* The key is 't', thus we draw both the 'front' and the 'back' sides of the triangles in the triangle strip, approximating the custom shape of interest. */
 		face=GL_FRONT_AND_BACK;
-		cout<<"\tRendering both the 'front' and the 'back' sides of the triangles in the triangle strip."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		case 'I':
 
-		/* The key is 'I' */
+		/* The key is 'I', thus we completely render the triangles in the triangle strip (filled version), approximating the custom shape of interest. */
 		mode=GL_FILL;
-		cout<<"\tRendering completely the triangles in the triangle strip (filled version)."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		case 'i':
 
-		/* The key is 'i' */
+		/* The key is 'i', thus we completely render the triangles in the triangle strip (filled version), approximating the custom shape of interest. */
 		mode=GL_FILL;
-		cout<<"\tRendering completely the triangles in the triangle strip (filled version)."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		case 'L':
 
-		/* The key is 'L' */
+		/* The key is 'L', thus we render only the edges of the triangles in the triangle strip (wireframe version), approximating the custom shape of interest. */
 		mode=GL_LINE;
-		cout<<"\tRendering only the edges of the triangles in the triangle strip (wireframe version)."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		case 'l':
 
-		/* The key is 'l' */
+		/* The key is 'l', thus we render only the edges of the triangles in the triangle strip (wireframe version), approximating the custom shape of interest. */
 		mode=GL_LINE;
-		cout<<"\tRendering only the edges of the triangles in the triangle strip (wireframe version)."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		case 'P':
 		
-		/* The key is 'P' */
+		/* The key is 'P', thus we render only the points of the triangles in the triangle strip, approximating the custom shape of interest. */
 		mode=GL_POINT;
-		cout<<"\tRendering only the vertices of the triangles in the triangle strip."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		case 'p':
 		
-		/* The key is 'p' */
+		/* The key is 'p', thus we render only the points of the triangles in the triangle strip, approximating the custom shape of interest. */
 		mode=GL_POINT;
-		cout<<"\tRendering only the vertices of the triangles in the triangle strip."<<endl;
-		cout.flush();
-		eol=true;
 		glutPostRedisplay();
 		break;
 		
 		default:
 
-    	/* Other keys are not important for us */
+    	/* Other keys are not important for us! */
     	break;
 	}
 }
