57b178588c7755d6e92c538d54e273b6141770bf
David Canino <canino.david@gmail.com>
2017-07-05 00:24:08 +0200

    Minor/medium revisions for several tests

--- Example-045-old/src/main.cpp
+++ Example-045-old/src/main.cpp
@@ -92,8 +92,8 @@ int main(int argc,char **argv)
 	cout<<"\tx(r,s) = xc + R * cos(r) * cos(s), y(r,s) = yc + R * sin(r), z(r,s) = zc + R * cos(r) * sin(s)"<<endl<<endl<<"\tfor any 'R>0', for any 'r' in '[ 0, pi/2 ]', and for any 's' in '[ 0, 2*pi ]'."<<endl<<endl;
 	cout<<"\tHere, the 'Hemisphere' shape is approximated by a quad grid, formed by 'p' 'slices' around the Z-axis (similar to the longitudinal lines) and by 'q' 'stacks' along the Z-axis (similar to the latitudinal lines). Specifically, the 'wireframe versions' of the"<<endl;
 	cout<<"\telements in the quad grid (in 'blue') are rendered by using the perspective projection."<<endl<<endl;
-	cout<<"\tHere, the user cannot modify the 'radius' 'R', and the 'center' '(xc,yc,zc)', since they are fixed in advance. Instead, the user can modify the numbers 'p' and 'q' of the longitudinal and the latitudinal slices, respectively, as well as rotate the scene along"<<endl;
-	cout<<"\tthe coordinate axes. In particular the user can:"<<endl<<endl;
+	cout<<"\tIn this test, the user cannot modify the 'radius' 'R', and the 'center' '(xc,yc,zc)', since they are fixed in advance. Instead, the user can modify the numbers 'p' and 'q' of the longitudinal and the latitudinal slices, respectively, as well as rotate the"<<endl;
+	cout<<"\tscene along the coordinate axes. In particular the user can:"<<endl<<endl;
 	cout<<"\t\t-) increase the number 'p' of the longitudinal slices by pressing the 'P' key;"<<endl;
 	cout<<"\t\t-) decrease the number 'p' of the longitudinal slices by pressing the 'p' key;"<<endl;
 	cout<<"\t\t-) increase the number 'q' of the latitudinal slices by pressing the 'Q' key;"<<endl;
