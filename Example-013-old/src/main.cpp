fd17700f1a867c7954dc273eb2cbba86b97b7fd3
David Canino <canino.david@gmail.com>
2017-08-11 15:29:56 +0200

    Minor/medium revisions for several tests

--- Example-013-old/src/main.cpp
+++ Example-013-old/src/main.cpp
@@ -69,8 +69,8 @@ int main(int argc,char **argv)
 {
 	/* We initialize everything, and create a very basic window! */
 	cout<<endl<<"\tThis is the 'Example-013' Test, based on the (Old Mode) OpenGL."<<endl;
-	cout<<"\tIt draws several versions of the 'Square Annulus' shape in an OpenGL window. This shape is bounded by 2 (concentric) axis-parallel rectangles of different";
-	cout<<" size in the same spirit of a circular crown. It is often known as"<<endl;
+	cout<<"\tIt draws several versions of the 'Square Annulus' shape in an OpenGL window. This shape is bounded by '2' (concentric) axis-parallel rectangles of ";
+	cout<<"different size in the same spirit of a circular crown. It is often known as"<<endl;
 	cout<<"\tthe 'rectangular' crown. Its large rectangle is called the 'external' rectangle, and other rectangle is called the 'internal' rectangle."<<endl<<endl;
 	cout<<"\tHere, the 'Square Annulus' shape of interest is approximated by an unique triangle strip (in 'turquoise gray') without adding any 'Steiner' point. It is ";
 	cout<<"not possible to modify the size and the position for the 'external'"<<endl;
