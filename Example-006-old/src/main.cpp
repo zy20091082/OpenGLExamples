ab4f029e96b9d5bd946c21e9a7c654ffa886434e
David Canino <canino.david@gmail.com>
2017-08-24 09:26:29 +0200

    Minor/medium revisions for several tests

--- Example-006-old/src/main.cpp
+++ Example-006-old/src/main.cpp
@@ -5,12 +5,6 @@
  * 
  * Last update: August 2017
  *
- * This program is Free Software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.                                       
- *                                                                         
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License (http://www.gnu.org/licenses/gpl.txt) for more details.
- * 
  * main.cpp - the main function for the 'Example-006 (Old Mode)' Test.
  *******************************************************************************************************************************************************/
 
