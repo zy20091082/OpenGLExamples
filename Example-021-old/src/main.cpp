8f13874c79ce69a4dee1ab42ec39cedebbacd31a
David Canino <canino.david@gmail.com>
2017-09-09 11:37:52 +0200

    Minor/medium revisions for several tests

--- Example-021-old/src/main.cpp
+++ Example-021-old/src/main.cpp
@@ -3,14 +3,8 @@
  *
  * Main website (GitHub): http://github.com/davidcanino/OpenGLExamples
  * 
- * Last update: August 2017
+ * Last update: September 2017
  *
- * This program is Free Software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.                                       
- *                                                                         
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License (http://www.gnu.org/licenses/gpl.txt) for more details.
- * 
  * main.cpp - the main function for the 'Example-021 (Old Mode)' Test.
  *******************************************************************************************************************************************************/
 
@@ -18,6 +12,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <climits>
 #include <limits>
 #define PI 3.14159265358979324
 using namespace std;
@@ -37,22 +33,25 @@ using namespace std;
 
 #endif
 
-/// The radius <i>'R'</i> to be used for defining and drawing the polyline, approximating the <i>'Circle'</i> curve of interest.
+/// The radius <i>'R'</i> to be used for defining and drawing the (closed) polyline, approximating the <i>'Circle'</i> curve of interest.
 /**
- * Clearly, it must be a positive and not null floating-point value, provided interactively by the user.
+ * The value of this global variable is the radius 'R' to be used for defining and drawing the (closed) polyline, approximating the 'Circle' curve of interest. Clearly,
+ * the radius for the 'Cicle' curve of interest consists of a positive and not null floating-point value, that is provided interactively by the user.
  */
 float radius;
 
-/// The center coordinates <i>'(xc,yc)'</i> to be used for defining and drawing the polyline, approximating the <i>'Circle'</i> curve of interest.
+/// The center coordinates <i>'(xc,yc)'</i> to be used for defining and drawing the (closed) polyline, approximating the <i>'Circle'</i> curve of interest.
 /**
- * Clearly, the center for the 'Cicle' curve of interest is formed by 2 floating-point values, provided interactively by the user.
+ * The values of these global variables contain, respectively, the center coordinates '(xc,yc)' for the (closed) polyline, approximating the 'Circle' curve of interest.
+ * Clearly, the center coordinates for the 'Cicle' curve of interest consist of '2' floating-point values, that are provided interactively by the user.
  */
 float xc,yc;
 
-/// The number <i>'n'</i> of the vertices and the edges in the polyline, used for approximating the <i>'Circle'</i> curve of interest.
+/// The number <i>'n'</i> of the vertices (and also of the edges) to be used for defining the (closed) polyline, approximating the <i>'Circle'</i> curve of interest.
 /**
- * It is initially set to 'n=3', which is the minimum number 'n' of the vertices and the edges in the polyline of interest. It is interactively modified by pressing the
- * '+' and the '-' keys.
+ * The value of this global variable is the number 'n' of the vertices (and also of the edges) to be used for defining the (closed) polyline, approximating the 'Curve'
+ * curve of interest. The value of 'n', initially set to 'n=3', can be increased and decreased by pressing the '+' and the '-' keys, respectively. By construction, it
+ * is not possible to have 'n<3'.
  */
 unsigned int num_samples=3;
 
@@ -68,16 +67,25 @@ int main(int argc,char **argv)
 {
 	/* We initialize everything, and create a very basic window! */
 	cout<<endl<<"\tThis is the 'Example-021' Test, based on the (Old Mode) OpenGL."<<endl;
-	cout<<"\tIt draws a polyline (in 'red'), formed by an arbitrary number 'n' of the vertices and the edges, in an OpenGL window. The polyline of interest ";
-	cout<<"approximates the 'Circle' curve with radius 'R' and center '(xc,yc)'."<<endl;
-	cout<<"\tThe 'Circle' curve is defined as follows:"<<endl<<endl;
+	cout<<"\tIt draws a (closed) polyline (in 'red'), which is formed by an arbitrary number 'n>2' of the vertices (and thus also of the edges), in an OpenGL window.";
+	cout<<" The (closed) polyline of interest approximates the 'Circle' curve of radius 'R'"<<endl;
+	cout<<"\tand center coordinates '(xc,yc)'. The 'Circle' curve is defined as follows:"<<endl<<endl;
 	cout<<"\tx(t) = xc + R * cos(t), y(t) = yc + R * sin(t)"<<endl<<endl<<"\tfor any 'R>0', and for every 't' in '[-pi,pi]'."<<endl<<endl;
-	cout<<"\tThis test also provides a very basic interaction mechanism with the user, which must provide the radius 'R' and the center '(xc,yc)'. The user can also:";
+ 	cout<<"\tBroadly speaking, the 'Circle' curve is the boundary of the 'Circle' shape, containing all points at distance at most 'R' from the center '(xc,yc)'.";
 	cout<<endl<<endl;
-	cout<<"\t\t-) increase the number 'n' of the vertices and the edges in the polyline of interest by pressing the '+' key;"<<endl;
-	cout<<"\t\t-) decrease the number 'n' of the vertices and the edges in the polyline of interest by pressing the '-' key."<<endl<<endl;
+	cout<<"\tThis test also provides a very basic interaction mechanism with the user, which must provide interactively the radius 'R' (as a positive and not null ";
+	cout<<"floating-point value) and the center coordinates '(xc,yc)' (as '2' floating-point"<<endl;
+	cout<<"\tvalues). In this context, the user can also:"<<endl<<endl;
+	cout<<"\t\t-) increase the number 'n' of the vertices and the edges in the polyline of interest by pressing the '+' key. By construction, it is not possible to";
+	cout<<" have 'n<3'."<<endl;
+	cout<<"\t\t-) Decrease the number 'n' of the vertices and the edges in the polyline of interest by pressing the '-' key. By construction, it is not possible to";
+	cout<<" have 'n<3'."<<endl<<endl;
 	cout<<"\tLikewise, the window of interest can be closed by pressing any among the 'Q', the 'q', and the 'Esc' keys."<<endl<<endl;
+	cout<<"\t----------------------------------------------------------------------------------------------------------------------------------------------------------";
+	cout<<"------------------------------------------------------------------------------"<<endl<<endl;
 	cout.flush();
+
+	/* If we arrive here, then we can read the radius 'R' for the 'Circle' curve of interest. */
 	cout<<"\tPlease, insert the radius 'R' (thus, a positive and not null floating-point value) for the 'Circle' curve of interest: ";
 	cin>>radius;
 	if( (!cin) || (radius<=0) )
@@ -88,7 +96,7 @@ int main(int argc,char **argv)
 		pause();
 		return EXIT_FAILURE;
 	}
-	
+
 	/* Now, we read the center coordinates '(xc,yc)' for the 'Circle' curve of interest. */
 	cout<<"\tPlease, insert the center coordinates '(xc,yc)' for the 'Circle' curve of interest (thus, 2 floating-point values, separated by a space): ";
 	cout.flush();
@@ -102,7 +110,11 @@ int main(int argc,char **argv)
 		return EXIT_FAILURE;
 	}
 	
-	/* If we arrive here, then we can draw the polyline, approximating the 'Circle' curve! */
+	/* If we arrive here, then we can draw the (closed) polyline, approximating the 'Circle' curve! */
+	cout<<endl;
+	cout<<"\t----------------------------------------------------------------------------------------------------------------------------------------------------------";
+	cout<<"------------------------------------------------------------------------------"<<endl<<endl;
+	cout.flush();
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE);
 	glutInitWindowPosition(0,0);
@@ -116,6 +128,20 @@ int main(int argc,char **argv)
    	initialize(); 
 	glutMainLoop();
 	return EXIT_SUCCESS;
+}
+
+/// This function simulates a pause while this test runs.
+void pause()
+{
+	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	cin.clear();
+	cout << "\tPress the RETURN key to finish ... ";
+	cout.flush();
+	cin.get();
+	#ifndef _MSC_VER
+		cout << endl;
+		cout.flush();
+	#endif
 }
 
 /// This function updates the viewport for the scene when it is resized. */
@@ -136,7 +162,7 @@ void initialize()
 	/* We initialize the OpenGL window of interest! */
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 	num_samples=3;
-	cout<<endl<<"\tAt the beginning, the polyline, approximating the 'Circle' curve of center '("<<xc<<","<<yc<<")' and radius '"<<radius<<"', is formed by 'n=";
+	cout<<"\tAt the beginning, the (closed) polyline, approximating the 'Circle' curve of center '("<<xc<<","<<yc<<")' and radius '"<<radius<<"', is formed by 'n=";
 	cout<<num_samples<<"' vertices and 'n="<<num_samples<<"' edges (thus by the minimum number 'n' as possible)."<<endl<<endl;
 	cout.flush();
 }
@@ -149,63 +175,66 @@ void manageKeys(unsigned char key, int x, int y)
 	{
 		case 'q':
 	
-		/* The key is 'q', thus we can exit from this program. */
-		cout<<endl<<"\tThis program is closing correctly ... "<<endl<<endl;
-		pause();
-		exit(EXIT_SUCCESS);
-		break;
+			/* The key is 'q', thus we can exit from this program. */
+			cout<<endl<<"\tThis program is closing correctly ... "<<endl<<endl;
+			pause();
+			exit(EXIT_SUCCESS);
+			break;
 		
 		case 'Q':
 	
-		/* The key is 'Q', thus we can exit from this program. */
-		cout<<endl<<"\tThis program is closing correctly ... "<<endl<<endl;
-		pause();
-		exit(EXIT_SUCCESS);
-		break;
+			/* The key is 'Q', thus we can exit from this program. */
+			cout<<endl<<"\tThis program is closing correctly ... "<<endl<<endl;
+			pause();
+			exit(EXIT_SUCCESS);
+			break;
 		
 		case 27:
 	
-		/* The key is 'Esc', thus we can exit from this program. */
-		cout<<endl<<"\tThis program is closing correctly ... "<<endl<<endl;
-		pause();
-		exit(EXIT_SUCCESS);
-		break;
+			/* The key is 'Esc', thus we can exit from this program. */
+			cout<<endl<<"\tThis program is closing correctly ... "<<endl<<endl;
+			pause();
+			exit(EXIT_SUCCESS);
+			break;
 		
 		case '+':
 		
-		/* The key is '+', thus we increase the number 'n' of the vertices and the edges in the polyline of interest! */
-		num_samples=num_samples+1;
-		glutPostRedisplay();
-		break;
+			/* The key is '+', thus we increase the number 'n' of the vertices and the edges in the (closed) polyline of interest. By construction, it is not possible to
+			 * have 'n<3'. */
+			num_samples=num_samples+1;
+			glutPostRedisplay();
+			break;
 		
 		case '-':
 		
-		/* The key is '-', thus we decrease the number 'n' of the vertices and the edges (if possible) in the polyline of interest. */
-		if(num_samples>3) num_samples=num_samples-1;
-		else
-		{ 
-			cout<<"\tThe minimum number 'n=3' of the vertices and the edges in the polyline of interest is reached, and it is not possible to decrease again this ";
-			cout<<"number."<<endl;
-			cout.flush();
-		}
+			/* The key is '-', thus we decrease the number 'n' of the vertices and the edges (if possible) in the (closed) polyline of interest. By construction, it is
+			 * not possible to have 'n<3'. */
+			if(num_samples>3) num_samples=num_samples-1;
+			else
+			{
+				/* Here, we have already 'n=3', and it is not possible to decrease again the value of 'n'. */
+				cout<<"\tThe minimum number 'n=3' of the vertices and the edges in the (closed) polyline of interest is reached, and it is not possible to decrease ";
+				cout<<"again this number."<<endl;
+				cout.flush();
+			}
 
-		/* If we arrive here, then this case is finished! */
-		glutPostRedisplay();
-		break;
+			/* If we arrive here, then this case is finished! */
+			glutPostRedisplay();
+			break;
 
 		default:
 
-    	/* Other keys are not important for us! */
-    	break;
+    		/* Other keys are not important for us! */
+    		break;
 	}
 }
 
-/// This function draws the polyline (in <i>'red'</i>), approximating the <i>'Circle'</i> curve of interest, in the main OpenGL window.
+/// This function draws the (closed) polyline (in <i>'red'</i>), approximating the <i>'Circle'</i> curve of interest, in the main OpenGL window.
 void draw()
 {
 	float t;
 
-	/* We draw the polyline (in 'red'), approximating the 'Circle' curve of interest, in the main OpenGL window. */
+	/* We draw the (closed) polyline (in 'red'), approximating the 'Circle' curve of interest, in the main OpenGL window. */
 	t=0;
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0,0.0,0.0);
@@ -219,21 +248,7 @@ void draw()
 	/* If we arrive here, then all is ok */
 	glEnd();
 	glFlush();
-	cout<<"\tThe 'Circle' curve of interest is currently approximated by a polyline with 'n="<<num_samples<<"' vertices and 'n="<<num_samples<<"' edges."<<endl;
+	cout<<"\tThe 'Circle' curve of interest is currently approximated by a (closed) polyline with 'n="<<num_samples<<"' vertices and 'n="<<num_samples<<"' edges."<<endl;
 	cout.flush();
-}
-
-/// This function simulates a pause while this test runs.
-void pause()
-{
-	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	cin.clear();
-	cout << "\tPress the RETURN key to finish ... ";
-	cout.flush();
-	cin.get();
-	#ifndef _MSC_VER
-		cout << endl;
-		cout.flush();
-	#endif
 }
 
