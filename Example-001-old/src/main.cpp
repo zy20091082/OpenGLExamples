3336d4f0bd611689e510f8449c66c20319cd96ff
David Canino <canino.david@gmail.com>
2018-01-13 15:00:18 +0100

    Added a revised version of the 'Example-001-old' (to be completed)

---
+++ Example-001-old/src/main.cpp
@@ -0,0 +1,63 @@
+/*******************************************************************************************************************************************************
+ * David Canino (canino.david@gmail.com)
+ *
+ * Main website (GitHub): http://github.com/davidcanino/OpenGLExamples
+ *
+ * Last update: January 2018
+ *
+ * main.cpp - the main function for the 'Example-001 (Old Mode)' Test.
+ *******************************************************************************************************************************************************/
+
+/* First, we must understand which platform we are using. */
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+#include "GL/glew.h"
+#ifdef __APPLE__
+
+    /* We are using any MacOSX platform (Macintosh). */
+    #include "GLUT/glut.h"
+    #include "OpenGL/gl.h"
+
+#else
+
+    /* We are not using a MacOSX platform. For instance, we have either any generic Unix-like platform (e.g., any GNU/Linux distribution), or any Microsoft Windows platform. */
+    #include "GL/glut.h"
+    #include "GL/gl.h"
+
+#endif
+
+/* Forward prototype! */
+void draw(void);
+
+/// The main function for the <i>'Example-001 (Old Mode)'</i> Test.
+int main(int argc,char **argv)
+{
+    /* First, we put out a welcome message on the standard output stream! */
+    cout<<endl<<"\tThis is the 'Example-001' Test, based on the (Old Mode) OpenGL."<<endl;
+    cout<<"\tIt is similar to the classic 'Hello World' program for testing the correctness of the building infrastructure."<<endl;
+    cout<<"\tBroadly speaking, it creates an empty OpenGL window with the 'black' background, and provides no interaction mechanism with the user."<<endl<<endl;
+    cout<<"\tHere, the window of interest must be stopped through an enforced closure."<<endl<<endl;
+    cout<<"\t----------------------------------------------------------------------------------------------------------------------------------------------------------";
+    cout<<"------------------------------------------------------------------------------"<<endl;
+    cout.flush();
+    
+    /* If we arrive here, then we can create a rough window without user interaction. */
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE);
+    glutInitWindowPosition(50,25);
+    glutInitWindowSize(480,480);
+    glutCreateWindow("The 'Example-001' Test, based on the (Old Mode) OpenGL");
+    glutDisplayFunc(draw);
+    glutMainLoop();
+    return EXIT_SUCCESS;
+}
+
+/// This function draws only the <i>'black'</i> background in the OpenGL window of interest.
+void draw(void)
+{
+    /* We draw only the 'black' background in the OpenGL window of interest. */
+    glClearColor(0.0f,0.0f,0.0f,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+}
