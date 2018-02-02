5204141d1eed5524282dae55a90a96b20f17d2bb
David Canino <canino.david@gmail.com>
2018-02-03 00:08:52 +0100

    Minor revisions for several tests

---
+++ Example-006-old/src/main.cpp
@@ -0,0 +1,152 @@
+/*********************************************************************************************************************************************************
+ * David Canino (canino.david@gmail.com)
+ *
+ * Main website (GitHub): http://github.com/davidcanino/OpenGLExamples
+ *
+ * Last update: January 2018
+ *
+ * main.cpp - the main function for the 'Example-006 (Old Mode)' Test.
+ *********************************************************************************************************************************************************/
+
+/* First, we must understand which platform we are using. */
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+#include "GL/glew.h"
+#ifdef __APPLE__
+
+    /* We are using any MacOSX platform (Macintosh). */
+    #include "GLUT/glut.h"
+    #include "OpenGL/gl.h"
+
+#else
+
+    /* We are not using a MacOSX platform. For instance, we have either any generic Unix-like platform (e.g., any GNU/Linux distribution), or any Microsoft Windows platform. */
+    #include "GL/glut.h"
+    #include "GL/gl.h"
+
+#endif
+
+/* Forward prototypes for all functions of interest! */
+void draw();
+void initialize();
+void resize(int w,int h);
+void manageKeys(unsigned char key, int x, int y);
+
+/// The main function for the <i>'Example-006 (Old Mode)'</i> Test.
+int main(int argc,char **argv)
+{
+    /* First, we export out an introductory message for this test on the standard output stream (cout)! */
+    cout<<endl<<"\tThis is the 'Example-006' Test, based on the (Old Mode) OpenGL."<<endl;
+    cout<<"\tIt draws the 'filled version' of a polygon (in 'red') in an OpenGL window, and provides a basic interaction mechanism with the user."<<endl;
+    cout<<"\tThe polygon of interest is the intersection with the maximum number '7' of the edges and the vertices between the following elements:"<<endl<<endl;
+    cout<<"\t\t-) the ortographic viewing box '[0,100]' x '[0,100]' x '[-1,1]';"<<endl;
+    cout<<"\t\t-) the triangle with vertices '(-50,-30,0)' - '(150,-50,0)' - '(50,150,0)'."<<endl<<endl;
+    cout<<"\tHere, the window of interest can be closed by pressing any among the 'Q', the 'q', and the 'Esc' keys."<<endl<<endl;
+    cout<<"\t----------------------------------------------------------------------------------------------------------------------------------------------------------";
+    cout<<"------------------------------------------------------------------------------"<<endl<<endl;
+    cout.flush();
+
+    /* If we arrive here, then we draw the current scene! */
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE);
+    glutInitWindowPosition(0,0);
+    glutInitWindowSize(480,480);
+    glutCreateWindow("The 'Example-006' Test, based on the (Old Mode) OpenGL");
+    glutDisplayFunc(draw);
+    glutReshapeFunc(resize);
+    glutKeyboardFunc(manageKeys);
+    glewExperimental = GL_TRUE;
+    glewInit();
+    initialize();
+    glutMainLoop();
+    return EXIT_SUCCESS;
+}
+
+/// This function initializes the scene in the OpenGL window of interest.
+void initialize() { glClearColor(1.0, 1.0, 1.0, 0.0); }
+
+/// This function updates the viewport for the scene when it is resized. */
+void resize(int w, int h)
+{
+    /* We update the projection and the modeling matrices! */
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0,100,0,100,-1,1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+/// This function draws the <i>'filled version'</i> of the polygon (in <i>'red'</i>), which is the intersection with the maximum number <i>'7'</i> of the edges and the vertices between a triangle and the ortographic viewing box, in the OpenGL window of interest.
+void draw()
+{
+    /* We draw the 'filled' version' of a polygon (in 'red') in the OpenGL window of interest. It is the intersection with the maximum number '7' of the edges and the vertices between:
+     *
+     * -) the ortographic viewing box '[0,100]' x '[0,100]' x '[-1,1];
+     * -) the triangle with vertices '(-50,-30,0)' - '(150,-50,0)' - '(50,150,0).
+     */
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_POLYGON);
+    glVertex3f(-50,30,0);
+    glVertex3f(150,-50,0);
+    glVertex3f(50,150,0);
+    glEnd();
+    glFlush();
+}
+
+/// This function is the keyboard input processing routine for the OpenGL window of interest.
+void manageKeys(unsigned char key, int x, int y)
+{
+    /* We are interested only in the 'q' - 'Q' - 'Esc' keys. */
+    switch (key)
+    {
+        case 'q':
+            
+            /* The user presses the 'q' key, thus we can exit from this program! */
+            cout<<"\tThis program is closing correctly ... "<<endl<<endl;
+            cout<<"\tPress the RETURN key to finish ... ";
+            cout.flush();
+            cin.get();
+            #ifndef _MSC_VER
+                cout << endl;
+                cout.flush();
+            #endif
+            exit(EXIT_SUCCESS);
+            break;
+            
+        case 'Q':
+            
+            /* The user presses the 'Q' key, thus we can exit from this program! */
+            cout<<"\tThis program is closing correctly ... "<<endl<<endl;
+            cout<<"\tPress the RETURN key to finish ... ";
+            cout.flush();
+            cin.get();
+            #ifndef _MSC_VER
+                cout << endl;
+                cout.flush();
+            #endif
+            exit(EXIT_SUCCESS);
+            break;
+            
+        case 27:
+            
+            /* The user presses the 'Esc' key, thus we can exit from this program! */
+            cout<<"\tThis program is closing correctly ... "<<endl<<endl;
+            cout<<"\tPress the RETURN key to finish ... ";
+            cout.flush();
+            cin.get();
+            #ifndef _MSC_VER
+                cout << endl;
+                cout.flush();
+            #endif
+            exit(EXIT_SUCCESS);
+            break;
+            
+        default:
+            
+            /* Other keys are not important for us! */
+            break;
+    }
+}
